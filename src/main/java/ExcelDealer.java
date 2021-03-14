3a8f4599a5996c2b3b810ec1177e6a38c86f4270
pmmpa2-iscte <pmmpa2@iscte-iul.pt>
2021-03-14 23:31:19 +0000

    Excel reading and write methods

--- src/main/java/ExcelDealer.java
+++ src/main/java/ExcelDealer.java
@@ -1,14 +1,13 @@
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.File; 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -18,43 +17,93 @@ public class ExcelDealer {
 	private String excel_file;
 	private OPCPackage packet;
 	private XSSFWorkbook wb;
-	private XSSFSheet sheet;
+	private static XSSFSheet sheet;
+	private List<Integer> ignoredIndexes;
 
-	public ExcelDealer(String filename) {
+	public ExcelDealer(String filename, boolean read) {
 		excel_file = filename;
+		ignoredIndexes = new ArrayList<>();
 		try {
-			packet = OPCPackage.open(new File(excel_file));
-			wb = new XSSFWorkbook(packet);
-			sheet = wb.getSheetAt(0);
-		} catch (InvalidFormatException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			if (read) {
+				packet = OPCPackage.open(new File(excel_file));
+				wb = new XSSFWorkbook(packet);
+				sheet = wb.getSheetAt(0);
+				ignoredIndexes.add(7);
+				ignoredIndexes.add(10);
+				readFile();
+				// System.out.println(getAllCellsOfRow(3));
+				// System.out.println(getClassMethods(3,"ParsingException"));
+			} else {
+				wb = new XSSFWorkbook();
+				// packet = OPCPackage.open(new File(excel_file));
+
+			}
+		} catch (InvalidFormatException | IOException e) {
 			e.printStackTrace();
 		}
 
 	}
 
 	public void readFile() {
-
-		XSSFSheet sheet = wb.getSheetAt(0);
-		XSSFRow row;
-		XSSFCell cell;
-
-		System.out.println(getMethods(0, "ParsingException"));
-		System.out.println(getClasses());
-
+//		// Primeiro argumento � a coluna do Excel e o segundo � a classe, d� para ir
+//		// buscar al�m dos m�todos
+//		System.out.println("LOC: " + getTotal(5));
+//		System.out.println(getMethods(3, "ParsingException"));
+//		//System.out.println("PACKAGES: " + getTotal(1).size());
+//		System.out.println("CLASSES: " + getClasses().size());
+//		System.out.println("MÉTODOS: " + getTotal(3).size());
 	}
 
-	public List<String> getMethods(int col_index, String name) {
+	public void createExcelFile(String file_name, String pathToSave, List<String[]> rows) {
+		File file = new File(pathToSave + "\\" + file_name);
+		file.delete();
+		XSSFSheet sheet = wb.createSheet(file_name);
+		String[] titles = { "MethodID", "package", "class", "method", "NOM_class", "LOC_class", "WMC_class",
+				"is_God_Class", "LOC_method", "CYCLO_method", "is_Long_Method" };
+
+		CellStyle style = wb.createCellStyle();
+		Font font = wb.createFont();
+		font.setBold(true);
+		style.setFont(font);
+
+		XSSFRow row = sheet.createRow(0);
+		for (int i = 0; i != titles.length; i++) {
+			XSSFCell cell = row.createCell(i);
+			cell.setCellStyle(style);
+			cell.setCellValue(titles[i]);
+		}
+
+		for (int i = 0; i != rows.size(); i++) {
+			row = sheet.createRow(i + 1);
+			XSSFCell cell = row.createCell(0);
+			cell.setCellValue(i + 1);
+			for (int j = 0; j != rows.get(i).length; j++) {
+				cell = row.createCell(j + 1);
+				cell.setCellValue(rows.get(i)[j]);
+			}
+		}
+		for (int i = 0; i != titles.length; i++)
+			sheet.autoSizeColumn(i);
+
+		try (FileOutputStream outputStream = new FileOutputStream(
+				new String(pathToSave + "\\" + file_name + ".xlsx"))) {
+			wb.write(outputStream);
+			outputStream.close();
+		} catch (IOException e) {
+
+		}
+	}
+
+////////////////////                   READING METHODS                   ////////////////////                     
+
+	public List<String> getClassMethods(int col_index, String classname) {
 		List<String> list = new ArrayList<>();
 
 		for (int r = 0; r < sheet.getPhysicalNumberOfRows(); r++) {
 			XSSFRow row = sheet.getRow(r);
 			if (row != null) {
-				if (row.getCell(2).getStringCellValue().equals(name))
-					if (!list.contains(row.getCell(col_index).toString()))
+				if (row.getCell(2).getStringCellValue().equals(classname))
+					if (row.getCell(col_index) != null && !list.contains(row.getCell(col_index).toString()))
 						list.add(row.getCell(col_index).toString());
 			}
 		}
@@ -74,33 +123,78 @@ public class ExcelDealer {
 		return list;
 	}
 
+	public List<Object[]> getAllRows() {
+		int row_size = sheet.getRow(0).getPhysicalNumberOfCells();
+		List<Object[]> list = new ArrayList<>();
+		for (int j = 1; j < sheet.getPhysicalNumberOfRows(); j++) {
+			Object[] rowList = new Object[row_size - 2]; // -2 para ignorar as colunas dos booleans que o stor não quer
+			XSSFRow row = sheet.getRow(j);
+			if (row != null) {
+				int counter = 0;
+				for (int i = 0; i <= row_size; i++) {
+					if (!ignoredIndexes.contains(i) && counter < rowList.length) {
+						rowList[counter] = row.getCell(i);
+						counter++;
+					}
+				}
+				list.add(rowList);
+			}
+		}
+		return list;
+	}
+
+	public Object[] getExcelHeader() {
+		int row_size = sheet.getRow(0).getPhysicalNumberOfCells();
+		Object[] rowList = new Object[row_size - 2];
+		XSSFRow row = sheet.getRow(0);
+		int counter = 0;
+		for (int i = 0; i < row_size; i++) {
+			if (!ignoredIndexes.contains(i) && counter < rowList.length) {
+				rowList[counter] = row.getCell(i);
+				counter++;
+			}
+
+		}
+		return rowList;
+	}
+
+	public List<String> getAllCellsOfRow(int column_index) {
+		List<String> list = new ArrayList<>();
+		for (String classname : getClasses()) {
+			List<String> aux = getClassMethods(column_index, classname);
+			for (String str : aux) {
+				if (!list.contains(str) || column_index == 3)
+					list.add(str);
+			}
+		}
+		return list;
+	}
+
+	public int sumLinesOfCode() {
+		List<String> list = getAllCellsOfRow(5);
+		double total = 0;
+
+		for (int i = 0; i < list.size(); i++)
+			total += Double.parseDouble(list.get(i));
+		return (int) total;
+	}
+
 	public String getExcel_file() {
 		return excel_file;
 	}
 
-	public void setExcel_file(String excel_file) {
-		this.excel_file = excel_file;
-	}
-
-	public OPCPackage getPacket() {
-		return packet;
-	}
-
-	public void setPacket(OPCPackage packet) {
-		this.packet = packet;
-	}
-
-	public XSSFWorkbook getWb() {
-		return wb;
-	}
-
-	public void setWb(XSSFWorkbook wb) {
-		this.wb = wb;
-	}
-
 	public static void main(String[] args) {
-		ExcelDealer er = new ExcelDealer("Code_Smells.xlsx");
-		er.readFile();  
+		ExcelDealer er = new ExcelDealer("Code_Smellss", false);
+		ExcelDealer er1 = new ExcelDealer("Code_Smells", true);
+		er1.readFile();
+	}
+
+	public void addAllToIgnoredIndexes(List<Integer> indexes) {
+		ignoredIndexes.addAll(indexes);
+	}
+
+	public void addToIgnoredIndexes(int index) {
+		ignoredIndexes.add(index);
 	}
 
 }
---
+++ src/main/java/NOM.java
@@ -0,0 +1,79 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class NOM {
+
+	private static Scanner scanner;
+
+	public static List<String> countMethods(File file) {
+		String[] classname = file.getName().split("[.]");
+		String regex = "(public|protected|private|static)+\\n* \\s*[\\w\\<\\>\\[\\]\\.]+\\n*\\s*(\\w+) *\\n*\\s*\\([^\\)]*\\) *(\\{?|[^;])";
+		String regex2 = "(public|protected|private|static)+\\n* \\s*(\\w+) *\\n*\\s*\\([^\\)]*\\) *(\\{?|[^;])";
+		String regex3 = "(public|protected|private|static)+\\n* \\s*" + "(" + classname[0] + ")"
+				+ " *\\n*\\s*\\([^\\)]*\\) *(\\{?|[^;])";
+		String regex4 = "^\\s*[\\w\\<\\>\\[\\]\\.]+\\n* \\s*(\\w+) *\\n*\\s*\\([^\\)]*\\)* *(\\{?|[^;])";
+		String regex5 = "(if|else|for|while|switch|catch)\\n* \\s*(\\w+) \\n*\\s*\\([^\\)]*\\)* *(\\{?|[^;])";
+		Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+		Pattern pattern2 = Pattern.compile(regex2, Pattern.MULTILINE);
+		Pattern pattern3 = Pattern.compile(regex3, Pattern.MULTILINE);
+		Pattern pattern4 = Pattern.compile(regex4, Pattern.MULTILINE);
+		Pattern pattern5 = Pattern.compile(regex5, Pattern.MULTILINE);
+		
+		List<String> list = new ArrayList<String>();
+		List<String> list2 = new ArrayList<>();
+
+		try {
+			scanner = new Scanner(file,"cp1252");
+			String text = scanner.useDelimiter("\\A").next();
+			String text2 = text.replaceAll("(\\\"(.*)\\\"|\\/\\/(.*)|\\/\\*(.*)|\\*\\/(.*))", "");
+			Matcher matcher = pattern.matcher(text2);
+			Matcher matcher4 = pattern4.matcher(text2);
+			while (matcher.find()) {
+
+				list.add(matcher.group().trim());
+			}
+
+			while (matcher4.find()) {
+				Matcher matcher5 = pattern5.matcher(matcher4.group());
+				if (!matcher5.find()) {
+					list.add(matcher4.group().trim());
+				}
+			}
+
+			List<String> list3 = list.stream().distinct().collect(Collectors.toList());
+			for (String str : list3) {
+				Matcher matcher6 = pattern2.matcher(str);
+				if (matcher6.find()) {
+					Matcher matcher7 = pattern3.matcher(str);
+					if (!matcher7.find()) {
+						list2.add(str);
+					}
+				}
+			}
+
+			list3.addAll(list2);
+			list3 = list3.stream()
+					.collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
+					.entrySet().stream().filter(e -> e.getValue() == 1).map(Map.Entry::getKey)
+					.collect(Collectors.toList());
+			
+			return list3;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			scanner.close();
+		}
+		
+		return null;
+	}
+}
