39b571c8edded60f8244a97c43bdd1c2508276ca
PedroPinheiroLX <pedro.pinheiro.f21@gmail.com>
2021-03-20 15:23:32 +0000

    Código mais simples e lê abstract

--- NOM_Class/NumberOfMethods.java
+++ NOM_Class/NumberOfMethods.java
@@ -1,76 +1,46 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.HashSet;
 import java.util.Scanner;
-import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
+
 
 public class NumberOfMethods {
 
 	private static Scanner scanner;
-	private static List<String> list = new ArrayList<String>();
-	private static List<String> list2 = new ArrayList<>();
+	private static HashSet<String> nomMethod = new HashSet<String>();
 
 	public static void countMethods(String filepath) {
 		File file = new File(filepath);
-		String[] classname = file.getName().split("[.]");
-		String regex = "(public|protected|private|static)+\\n* \\s*[\\w\\<\\>\\[\\]\\.]+\\n*\\s*(\\w+) *\\n*\\s*\\([^\\)]*\\) *(\\{?|[^;])";
-		String regex2 = "(public|protected|private|static)+\\n* \\s*(\\w+) *\\n*\\s*\\([^\\)]*\\) *(\\{?|[^;])";
-		String regex3 = "(public|protected|private|static)+\\n* \\s*" + "(" + classname[0] + ")"
-				+ " *\\n*\\s*\\([^\\)]*\\) *(\\{?|[^;])";
-		String regex4 = "^\\s*[\\w\\<\\>\\[\\]\\.]+\\n* \\s*(\\w+) *\\n*\\s*\\([^\\)]*\\)* *(\\{?|[^;])";
-		String regex5 = "(if|else|for|while|switch|catch)\\n* \\s*(\\w+) \\n*\\s*\\([^\\)]*\\)* *(\\{?|[^;])";
+		String regex = "(public|protected|private|static)+\\n*\\s*(abstract)?\\n*\\s*[\\w\\<\\>\\[\\]\\.]+\\n*\\s*(\\w+)\\n*\\s*\\([^\\)]*\\) *(\\{?|[^;])";
+		String regex2 = "^\\s*(abstract)?\\n*\\s*[\\w\\<\\>\\[\\]\\.]+\\n* \\s*(\\w+) *\\n*\\s*\\([^\\)]*\\)* *(\\{?|[^;])";
+		String regex3 = "(if|else|for|while|switch|catch)\\n* \\s*(\\w+) \\n*\\s*\\([^\\)]*\\)* *(\\{?|[^;])";
 		Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
 		Pattern pattern2 = Pattern.compile(regex2, Pattern.MULTILINE);
 		Pattern pattern3 = Pattern.compile(regex3, Pattern.MULTILINE);
-		Pattern pattern4 = Pattern.compile(regex4, Pattern.MULTILINE);
-		Pattern pattern5 = Pattern.compile(regex5, Pattern.MULTILINE);
 
 		try {
 			scanner = new Scanner(file);
 			String text = scanner.useDelimiter("\\A").next();
-			String text2 = text.replaceAll("(\\\"(.*)\\\"|\\/\\/(.*)|\\/\\*(.*)|\\*\\/(.*))", "");
-			Matcher matcher = pattern.matcher(text2);
-			Matcher matcher4 = pattern4.matcher(text2);
+			String cleanText = text.replaceAll("(\\\"(.*)\\\"|\\/\\/(.*)|\\/\\*(.*)|\\*\\/(.*))", "");
+			Matcher matcher = pattern.matcher(cleanText);
+			Matcher matcher2 = pattern2.matcher(cleanText);
 			while (matcher.find()) {
-
-				list.add(matcher.group().trim());
+				nomMethod.add(matcher.group().trim().replace("{", ""));
 			}
 
-			
-
-			while (matcher4.find()) {
-				Matcher matcher5 = pattern5.matcher(matcher4.group());
-				if (!matcher5.find()) {
-					list.add(matcher4.group().trim());
+			while (matcher2.find()) {
+				Matcher matcher3 = pattern3.matcher(matcher2.group());
+				if (!matcher3.find()) {
+					nomMethod.add(matcher2.group().trim().replace("{", ""));
 				}
+
 			}
 
-			List<String> list3 = list.stream().distinct().collect(Collectors.toList());
-			for (String str : list3) {
-				Matcher matcher6 = pattern2.matcher(str);
-				if (matcher6.find()) {
-					Matcher matcher7 = pattern3.matcher(str);
-					if (!matcher7.find()) {
-						list2.add(str);
-					}
-				}
-			}
-
-			list3.addAll(list2);
-			list3 = list3.stream()
-					.collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
-					.entrySet().stream().filter(e -> e.getValue() == 1).map(Map.Entry::getKey)
-					.collect(Collectors.toList());
-
-			System.out.println(Arrays.toString(list3.toArray()));
-			System.out.println(list3.size());
+			System.out.println(Arrays.toString(nomMethod.toArray()));
+			System.out.println(nomMethod.size());
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} finally {
@@ -78,9 +48,10 @@ public class NumberOfMethods {
 		}
 	}
 
-	
+	@SuppressWarnings("unused")
 	public static void main(String[] args) throws FileNotFoundException {
 		String str = "C:\\Users\\Pedro Pinheiro\\Downloads\\jasml_0.10\\src\\com\\jasml\\compiler\\SourceCodeParser.java";
-		countMethods(str);
+		String str2 = "C:\\Users\\Pedro Pinheiro\\Desktop\\PCD - 2º Ano LEI (ISCTE)\\Eclipse PCD Trabalhos\\Projeto Graficos 88657.zip_expanded\\ProjetoIP_Graficos\\src\\Projeto\\Grafico.java";
+		countMethods(str2);
 	}
 }
