06641cd5039ebc47933a420264a05e0013118a81
SyhkoCode <61979258+SyhkoCode@users.noreply.github.com>
2021-04-14 22:47:48 +0100

    Column index getters for isGodClass and isLongMethod

--- src/main/java/metrics/CompareFiles.java
+++ src/main/java/metrics/CompareFiles.java
@@ -24,6 +24,28 @@ public class CompareFiles {
 
 
 
+	private int get_isGodClass_ColIndex(XSSFSheet sheet) {
+		int isGodClassIndex = 0;
+		for (Cell s : sheet.getRow(0)) {
+			if (s.getStringCellValue().toLowerCase().equals("is_god_class")) {
+				isGodClassIndex = s.getColumnIndex();
+			}
+		}
+		return isGodClassIndex;
+	}
+	
+	private int get_isLongMethod_ColIndex(XSSFSheet sheet) {
+		int isLongMethodIndex = 0;
+		for (Cell s : sheet.getRow(0)) {
+			if (s.getStringCellValue().toLowerCase().equals("is_long_method")) {
+				isLongMethodIndex = s.getColumnIndex();
+			}
+		}
+		return isLongMethodIndex;
+	}
 
+	public static void main(String[] args) {
+		CompareFiles cf = new CompareFiles("F:\\Google Drive\\ISCTE\\ANO 3\\ES\\Code_Smells.xlsx", "C:\\Users\\sophi\\Desktop\\jasml_0.10_metrics.xlsx");
+	}
 
 }
