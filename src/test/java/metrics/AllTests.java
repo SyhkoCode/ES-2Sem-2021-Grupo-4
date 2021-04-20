2936189a22f008b4cea12e11d6c2222499908993
SyhkoCode <61979258+SyhkoCode@users.noreply.github.com>
2021-04-20 11:49:27 +0100

    IndicatorTest 100%

--- src/test/java/metrics/AllTests.java
+++ src/test/java/metrics/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import org.junit.platform.suite.api.SelectClasses;
 
 @RunWith(JUnitPlatform.class)
-@SelectClasses({MetricsTest.class})
+@SelectClasses({MetricsTest.class,IndicatorTest.class})
 public class AllTests {
 
 }
