ef7b4db7b6565cf5bca0d6249e1f7012bf59fda1
SyhkoCode <61979258+SyhkoCode@users.noreply.github.com>
2021-04-20 12:17:00 +0100

    ReadJavaProjectTest 97,5%
    will never catch IOException

---
+++ src/test/java/metrics/ReadJavaProjectTest.java
@@ -0,0 +1,39 @@
+package metrics;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ReadJavaProjectTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	final void testReadJavaProject() {
+		List<String[]> result = ReadJavaProject.readJavaProject("src\\test\\resources\\jasml_0.10");
+		assertNotNull(result);
+	}
+
+}
