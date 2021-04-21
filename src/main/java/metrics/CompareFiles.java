ba719c684fb4156ceb0d2bdbe80ff30bce816287
SyhkoCode <61979258+SyhkoCode@users.noreply.github.com>
2021-04-21 20:24:03 +0100

    new class Quality + better CompareFiles + complete Javadoc annotations Created new class Quality and rearranged CompareFiles accordingly for better code design. Also, wrote all Javadoc annotations for Quality, CompareFiles and Indicator.

--- src/main/java/metrics/CompareFiles.java
+++ src/main/java/metrics/CompareFiles.java
@@ -12,6 +12,12 @@ import java.util.List;
 
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 
+/**
+ * Allows to compare between different files to be able to evaluate the quality of code smell detection
+ * @author Sofia Chaves, Pedro Pinheiro
+ * @version 2.0
+ * @since   2021-04-15 
+ */
 @SuppressWarnings("unused")
 public class CompareFiles {
 
@@ -23,9 +29,12 @@ public class CompareFiles {
 	private ExcelDealer excelDealerCreated;
 	private boolean booleanColumnsFilled;
 
-	HashMap<String,Indicator> saveIndsPerMethod = new HashMap<>();
-	HashMap<String,Indicator> saveIndsPerClass = new HashMap<>();
-
+	/**
+	 * Constructor given two files
+	 * This is for the situation when User chose to save an Excel file with his rules.
+	 * @param csFileDefault Path of Excel file after User submits project.
+	 * @param csFileCreated Path of Excel file created when User also submitted and saved rules, which will have code smells columns filled accordingly.
+	 */
 	public CompareFiles(String csFileDefault, String csFileCreated) {
 		this.csFileDefault = csFileDefault;
 		this.csFileCreated = csFileCreated;
@@ -34,6 +43,13 @@ public class CompareFiles {
 		this.booleanColumnsFilled = true;
 	}
 
+	/**
+	 * Constructor given three files
+	 * This is for the situation when User chose not to save an Excel file with his rules.
+	 * @param csFileDefault Path of Excel file after User submits project.
+	 * @param metricsFile Path of Excel file with code smells columns not filled.
+	 * @param rulesFile Path of text file after User submits rules.
+	 */
 	public CompareFiles(String csFileDefault, String metricsFile, String rulesFile) {
 		this.csFileDefault = csFileDefault;
 		this.metricsFile = metricsFile;
@@ -43,7 +59,12 @@ public class CompareFiles {
 		this.booleanColumnsFilled = false;
 	}
 
-	public LinkedHashMap<String, Integer> getColIndexesByTitles(String[] titles) {
+	/**
+	 * Gets the corresponding Column Indexes of given Strings that represent Column Titles in two Excel files
+	 * @param titles These are the Title Columns required to get their indexes.
+	 * @return LinkedHashMap<String, Integer> in which the String is the Column Title and the Integer is the corresponding Column Index per Excel file
+	 */
+	private LinkedHashMap<String, Integer> getColIndexesByTitles(String[] titles) {
 		LinkedHashMap<String, Integer> indexesMap = new LinkedHashMap<>();
 
 		for (String title : titles) {
@@ -61,6 +82,13 @@ public class CompareFiles {
 		return indexesMap;
 	}
 	
+	/**
+	 * Compares between two strings to return the right Indicator for each situation.
+	 * Is used to compare String values of two Excel cells in method compareWith2Files and to compare String values of one Excel cell and one MethodRuleAnalysis map value in method compareWith3Files.
+	 * @param defaultText Given string.
+	 * @param createdText Given string.
+	 * @return Indicator This is the right Indicator after comparison of given strings.
+	 */
 	private static Indicator parseIndicator(String defaultText, String createdText) {
 		List<String> valid = Arrays.asList("TRUE", "FALSE");
 		if (!valid.contains(defaultText) || !valid.contains(defaultText)) {
@@ -69,8 +97,15 @@ public class CompareFiles {
 		return (defaultText.equals("TRUE") ? (createdText.equals("TRUE") ? Indicator.VP : Indicator.FN) : (createdText.equals("TRUE") ? Indicator.FP : Indicator.VN));
 	}
 
-	public void compareWith2Files(String[] titles) {
+	/**
+	 * When CompareFiles receives 2 Excel files, this is the method that allows the comparison between them and the code smell detection quality evaluation.
+	 * @param titles These are the Title Columns required to get their indexes.
+	 * @return Quality object that contains both HashMaps with Indicators per Method and per Class
+	 */
+	private Quality compareWith2Files(String[] titles) {
 		HashMap<String, Integer> indexesMap = getColIndexesByTitles(titles);
+		HashMap<String,Indicator> saveIndsPerMethod = new HashMap<>();
+		HashMap<String,Indicator> saveIndsPerClass = new HashMap<>();
 		
 		for(Object[] objDefaultExcel : excelDealerDefault.getAllRows(0)) {
 
@@ -99,10 +134,17 @@ public class CompareFiles {
 						}
 					}
 				}
-			}	
+			}
+		return new Quality(saveIndsPerMethod,saveIndsPerClass);	
 	}
 	
-	public void compareWith3Files() {
+	/**
+	 * When CompareFiles receives 3 files, this is the method that allows the comparison between them and the code smell detection quality evaluation.
+	 * @return Quality object that contains both HashMaps with Indicators per Method and per Class
+	 */
+	private Quality compareWith3Files() {
+		HashMap<String,Indicator> saveIndsPerMethod = new HashMap<>();
+		HashMap<String,Indicator> saveIndsPerClass = new HashMap<>();
 		MethodRuleAnalysis mra = new MethodRuleAnalysis(MethodData.excelToMetricsMap(metricsFile),Rule.allRules(new File(rulesFile)));
 		
 		for(Object[] objDefaultExcel : excelDealerDefault.getAllRows(0)) {
@@ -130,123 +172,33 @@ public class CompareFiles {
 				}
 			}
 		}
+		return new Quality(saveIndsPerMethod,saveIndsPerClass);
 	}
 	
-	public List<HashMap<String, Indicator>> testQuality(String[] titles) {
-		List<HashMap<String, Indicator>> indicators = new ArrayList<>();
+	/**
+	 * Allows to evaluate the quality of code smell detection by choosing the right comparison method.
+	 * @param titles These are the Column Titles required to evaluate.
+	 * @return Quality object that contains both HashMaps with Indicators per Method and per Class
+	 */
+	public Quality testQuality(String[] titles) {
 		if (booleanColumnsFilled){
-			compareWith2Files(titles);
+			return compareWith2Files(titles);
 		} else {
-			compareWith3Files();
+			return compareWith3Files();
 		}
-		indicators.add(saveIndsPerClass);
-		indicators.add(saveIndsPerMethod);
-		return indicators;
-	}
-	
-	public List<HashMap<String, Indicator>> testQualityAntigo(String[] titles) {
-		HashMap<String, Integer> indexesMap = getColIndexesByTitles(titles);
-
-		for(Object[] objDefaultExcel : excelDealerDefault.getAllRows(0)) {
-
-			MethodData dataDefaultExcel = new MethodData(objDefaultExcel);
-
-			if (booleanColumnsFilled){
-
-				for (Object[] objCreatedExcel : excelDealerCreated.getAllRows(0)) {
-
-					MethodData dataCreatedExcel = new MethodData(objCreatedExcel);
-
-					if (dataDefaultExcel.getPackageName().equals(dataCreatedExcel.getPackageName())
-							&& dataDefaultExcel.getClassName().equals(dataCreatedExcel.getClassName())
-							&& dataDefaultExcel.getMethodName().equals(dataCreatedExcel.getMethodName())) {
-
-						int arrayIndex = 0;
-						for (int i = 0; i < titles.length; i++) {
-							String cellTextDefault = String.valueOf(objDefaultExcel[(int) indexesMap.values().toArray()[arrayIndex]]);
-							String cellTextCreated = String.valueOf(objCreatedExcel[(int) indexesMap.values().toArray()[arrayIndex + 1]]);
-							String classe = String.valueOf(objCreatedExcel[2]);
-							String metodo = String.valueOf(objCreatedExcel[3]);
-
-							Indicator indicator = parseIndicator(cellTextDefault, cellTextCreated);
-							saveIndsPerMethod.put(metodo, indicator);
-							saveIndsPerClass.put(classe, indicator);
-
-							arrayIndex += 2;
-						}
-					}
-				}
-			} else {
-				/* o mra estar dentro de cada iteracao por row do objDefaultExcel era o q estava a fazer demorar imenso tempo*/
-				MethodRuleAnalysis mra = new MethodRuleAnalysis(MethodData.excelToMetricsMap(metricsFile),Rule.allRules(new File(rulesFile)));
-				for (int i = 0; i < mra.getMethods().size(); i++) {
-
-					String god_class = String.valueOf(mra.getMap().get("is_God_Class").get(i)).toUpperCase();
-					String long_method = String.valueOf(mra.getMap().get("is_Long_Method").get(i)).toUpperCase();
-					
-					String cellTextDefault_godclass = String.valueOf(objDefaultExcel[7]);
-					String cellTextDefault_longmethod = String.valueOf(objDefaultExcel[10]);
-					String classe = dataDefaultExcel.getClassName();
-					String metodo = dataDefaultExcel.getMethodName();
-
-					if(dataDefaultExcel.getPackageName().equals(mra.getMethods().get(i).getPackageName()) 
-							&& dataDefaultExcel.getClassName().equals(mra.getMethods().get(i).getClassName())
-							&& dataDefaultExcel.getMethodName().equals(mra.getMethods().get(i).getMethodName())) {
-						
-						Indicator indicator_godclass = parseIndicator(cellTextDefault_godclass, god_class);
-						saveIndsPerClass.put(classe, indicator_godclass);
-						
-						Indicator indicator_longmethod = parseIndicator(cellTextDefault_longmethod, long_method);
-						saveIndsPerMethod.put(metodo, indicator_longmethod);
-					}
-				}
-			}
-		}
-		List<HashMap<String, Indicator>> indicators = new ArrayList<>();
-		indicators.add(saveIndsPerClass);
-		indicators.add(saveIndsPerMethod);
-		return indicators;
-	}
-	
-	public HashMap<String,Indicator> getIndicatorsPerClass(List<HashMap<String, Indicator>> indicators) {
-		return indicators.get(0);
-	}
-	
-	public HashMap<String,Indicator> getIndicatorsPerMethod(List<HashMap<String, Indicator>> indicators) {
-		return indicators.get(1);
-	}
-	
-	public long countIndicatorInMap(Indicator indicator, HashMap<String,Indicator> indicatorsMap) {
-		return indicatorsMap.values().stream().filter(v -> v.equals(indicator)).count();		
 	}
 
+	/**
+	 * este main vai desaparecer
+	 */
 	public static void main(String[] args) throws IOException {
-//		CompareFiles cf = new CompareFiles("F:\\Google Drive\\ISCTE\\ANO 3\\ES\\Code_Smells.xlsx", "C:\\Users\\sophi\\Desktop\\jasml_0.10_metrics.xlsx");
-//		
 //		CompareFiles cf = new CompareFiles("C:\\Users\\Pedro Pinheiro\\Downloads\\Code_Smells.xlsx", "C:\\Users\\Pedro Pinheiro\\Pictures\\jasml_0.10_metrics.xlsx","C:\\Users\\Pedro Pinheiro\\Desktop\\rules.txt" );
-//
-//		
-//		List<HashMap<String, Indicator>> indicators = cf.testQuality(new String[]{"is_god_class","is_long_method"});	
-//		HashMap<String,Indicator> indicatorsPerClassMap = cf.getIndicatorsPerClass(indicators);
-//		HashMap<String,Indicator> indicatorsPerMethodMap = cf.getIndicatorsPerMethod(indicators);
-//		
-//		System.out.println(indicatorsPerClassMap.entrySet());
-//		System.out.println(indicatorsPerMethodMap.entrySet());
-//		System.out.println("No de FPs: "+cf.countIndicatorInMap(Indicator.FP,indicatorsPerClassMap)+ " em "+indicatorsPerClassMap.size()+" classes");
-//		System.out.println("No de FPs: "+cf.countIndicatorInMap(Indicator.FP,indicatorsPerMethodMap)+ " em "+indicatorsPerMethodMap.size()+" metodos");
-
-		CompareFiles cf2 = new CompareFiles("F:\\Google Drive\\ISCTE\\ANO 3\\ES\\Code_Smells.xlsx", "C:\\Users\\sophi\\Desktop\\jasml_0.10_metrics.xlsx", "testeregras");
-		
-		List<HashMap<String, Indicator>> indicators = cf2.testQuality(new String[]{"is_god_class","is_long_method"});	
-		HashMap<String,Indicator> indicatorsPerClassMap = cf2.getIndicatorsPerClass(indicators);
-		HashMap<String,Indicator> indicatorsPerMethodMap = cf2.getIndicatorsPerMethod(indicators);
-		
-		System.out.println(indicatorsPerClassMap.entrySet());
-		System.out.println(indicatorsPerMethodMap.entrySet());
-		System.out.println("No de FPs: "+cf2.countIndicatorInMap(Indicator.FP,indicatorsPerClassMap)+ " em "+indicatorsPerClassMap.size()+" classes");
-		System.out.println("No de FPs: "+cf2.countIndicatorInMap(Indicator.FP,indicatorsPerMethodMap)+ " em "+indicatorsPerMethodMap.size()+" metodos");
-
-
+//		CompareFiles cf = new CompareFiles("F:\\Google Drive\\ISCTE\\ANO 3\\ES\\Code_Smells.xlsx", "C:\\Users\\sophi\\Desktop\\jasml_0.10_metrics.xlsx");
+		CompareFiles cf2 = new CompareFiles("F:\\Google Drive\\ISCTE\\ANO 3\\ES\\Code_Smells.xlsx", "C:\\Users\\sophi\\Desktop\\jasml_0.10_metrics.xlsx", "testeregras");		
+		Quality result = cf2.testQuality(new String[]{"is_god_class","is_long_method"});
+		result.getIndicatorsPerClass();	
+		System.out.println(result.getIndicatorsPerMethod());
+		System.out.println("No de FPs: "+result.countIndicatorInMethods(Indicator.FP)+" em "+ result.getIndicatorsPerMethod().size());
 	}
 
 }
