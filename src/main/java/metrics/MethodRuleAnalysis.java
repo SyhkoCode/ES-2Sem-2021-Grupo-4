7df332eb8dd7af3b6d096e44de82ab203f40d57a
Tiago Mendes <tapms@iscte-iul.pt>
2021-04-27 12:44:04 +0100

    MethodRuleAnalysis Flexible

--- src/main/java/metrics/MethodRuleAnalysis.java
+++ src/main/java/metrics/MethodRuleAnalysis.java
@@ -37,61 +37,91 @@ public class MethodRuleAnalysis {
 
 	public ArrayList<String[]> getResults() {
 		ArrayList<String[]> result = new ArrayList<>();
-		String[] header = getCodeSmellResults().get(0).get(0);
-		result.add(header);
+		ArrayList<String[]> headerList = new ArrayList<>();
+		String[] auxHeader = new String[map.keySet().size()];
+		int counterHeader = 0;
+		for(String s : map.keySet()) {
+			auxHeader[counterHeader] = s;
+			counterHeader++;
+		}
+		headerList.add(auxHeader);		
+//		System.out.println(Arrays.toString(headerList.get(0)));
+		result.add(headerList.get(0));
 		for (int i = 0; i < getMethods().size(); i++) {
-			String[] aux = new String[4];
+			String[] aux = new String[2 + map.size()];
 			aux[0] = getMethods().get(i).getClassName();
 			aux[1] = getMethods().get(i).getMethodName();
-
-//			System.out.println(aux[0] + " AQUI");
 			int counter = 2;
 			for (String nome : getMap().keySet()) {
 				aux[counter] = getMap().get(nome).get(i).toString();
 				counter++;
-//				System.out.println(getMap().get(nome).get(i));			
 			}
+			System.out.println(Arrays.toString(aux));
 			result.add(aux);
 		}
 
 		for (int i = 0; i < result.size(); i++) {
 			for (int j = 0; j < result.get(i).length; j++) {
-				System.out.println(result.get(i)[j]);
+//				System.out.println(result.get(i)[j]);
 			}
 		}
-
 		return result;
 
 	}
 
 	public ArrayList<ArrayList<String[]>> getCodeSmellResults() {
-		ArrayList<String> classesFound = new ArrayList<>();
-		String[] metrics = new String[map.size()];
+		ArrayList<String> classesAndCodeSmellsFound = new ArrayList<>();
 		ArrayList<ArrayList<String[]>> result = new ArrayList<>();
-		result.add(new ArrayList<String[]>());
-		result.add(new ArrayList<String[]>());
-		result.add(new ArrayList<String[]>());
+		result.add(new ArrayList<>());
+		result.add(new ArrayList<>());
+		result.get(0).add(classHeader());
+		result.get(1).add(methodHeader());
 		for (int i = 0; i < getMethods().size(); i++) {
-			String[] aux_classes = new String[2];
-			String[] aux_methods = new String[3];
-			if (!classesFound.contains(getMethods().get(i).getClassName()) && getMap().containsKey("is_God_Class")) {
-				metrics[0] = "is_God_Class";
-				aux_classes[0] = getMethods().get(i).getClassName();
-				classesFound.add(getMethods().get(i).getClassName());
-				aux_classes[1] = getMap().get("is_God_Class").get(i).toString();
-				result.get(1).add(aux_classes);
-			}
-			if (getMap().containsKey("is_Long_Method")) {
-				metrics[map.size()-1] = "is_Long_Method";
-				aux_methods[0] = Integer.toString(i + 1);
-				aux_methods[1] = getMethods().get(i).getMethodName();
-				aux_methods[2] = getMap().get("is_Long_Method").get(i).toString();
-				result.get(2).add(aux_methods);
-			}
-			result.get(0).add(metrics);
-		}
+			String[] aux_classes = new String[getClassSmells().size() + 1];
+			String[] aux_methods = new String[getMethodSmells().size() + 2];
 
+			if (!classesAndCodeSmellsFound.contains(getMethods().get(i).getClassName())) {
+				aux_classes[0] = getMethods().get(i).getClassName();
+				for (int j = 1; j < aux_classes.length; j++) {
+					aux_classes[j] = map.get(getClassSmells().get(j - 1)).get(i).toString();
+
+					if (j == aux_classes.length - 1) {
+						classesAndCodeSmellsFound.add(getMethods().get(i).getClassName());
+						result.get(0).add(aux_classes);
+//						System.out.println(Arrays.toString(aux_classes));
+					}
+				}
+			}
+
+			aux_methods[0] = Integer.toString(i + 1);
+			aux_methods[1] = getMethods().get(i).getMethodName();
+			for (int j = 2; j < aux_methods.length; j++) {
+				aux_methods[j] = map.get(getMethodSmells().get(j - 2)).get(i).toString();
+				if (j == aux_methods.length - 1) {
+					result.get(1).add(aux_methods);
+//					System.out.println(Arrays.toString(aux_methods));
+				}
+			}
+		}
 		return result;
+	}
+
+	private ArrayList<String> getClassSmells() {
+		ArrayList<String> aux = new ArrayList<String>();
+		for (String s : map.keySet()) {
+			if (s.contains("Class"))
+				aux.add(s);
+		}
+		return aux;
+	}
+
+	private ArrayList<String> getMethodSmells() {
+		ArrayList<String> aux = new ArrayList<String>();
+		for (String s : map.keySet()) {
+			if (s.contains("Method"))
+				aux.add(s);
+		}
+		return aux;
 	}
 
 //	public static void main(String[] args) {
