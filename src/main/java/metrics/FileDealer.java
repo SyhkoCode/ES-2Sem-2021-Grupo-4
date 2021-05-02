86434dbdf2e6e0a21675dfadc4d35f46df8419d0
dcgraca <diogocaldeiragraca2000@gmail.com>
2021-05-02 20:04:59 +0100

    FileDealer atualizado com comentários para JavaDoc

--- src/main/java/metrics/FileDealer.java
+++ src/main/java/metrics/FileDealer.java
@@ -8,7 +8,21 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Auxiliary class to deal with the rules files
+ * @author Tiago Mendes
+ * @author Pedro Pereira
+ * @version 1.0
+ * @since  2021-04-14 
+ */
+
 public class FileDealer {
+	
+	/**
+	 * Writes a new file and places it on the given path
+	 * @param path Where the user wants to save the file
+	 * @param write ArrayList of Strings to create the file
+	 */
 
 	public static void createFile(String path, ArrayList<String> write) {
 		try {
@@ -24,6 +38,12 @@ public class FileDealer {
 
 	}
 
+	/**
+	 * Reads the given rules file and returns it as an array of Strings
+	 * @param path Location of the rule file
+	 * @return Rule file as an array of Strings
+	 */
+	
 	public static String[] readFile(String path) {
 		String[] result = new String[2];
 		try {
