8f7910e3a2f198c6197daba4e7dacb47252372a0
SyhkoCode <61979258+SyhkoCode@users.noreply.github.com>
2021-05-05 00:12:16 +0100

    CompareFilesTest fixed

--- src/test/java/metrics/CompareFilesTest.java
+++ src/test/java/metrics/CompareFilesTest.java
@@ -12,21 +12,24 @@ import org.junit.jupiter.api.Test;
 
 class CompareFilesTest {
 		
-	File excelDefault = new File(getClass().getResource("/Code_Smells.xlsx").getFile());
+	File excelDefault = new File(getClass().getResource("/jasml_0.10_metrics.xlsx").getFile());
 	String csFileDefault = excelDefault.getAbsolutePath();
 	
-	File excelCreated = new File(getClass().getResource("/jasml_0.10_metrics.xlsx").getFile());
-	String csFileCreated = excelCreated.getAbsolutePath();
+	File excelCreatedCSBooleans = new File(getClass().getResource("/jasml_csbooleans.xlsx").getFile());
+	String csFileCreatedBooleans = excelCreatedCSBooleans.getAbsolutePath();
+	
+	File excelCreated = new File(getClass().getResource("/jasml_0.10_metrics_created.xlsx").getFile());
+	String csFileCreated_metrics = excelCreated.getAbsolutePath();
 	
 	File testeregras = new File(getClass().getResource("/testeregras.txt").getFile());
 	String testeregrasPath = testeregras.getAbsolutePath();
 	
-	File excelDefault_MalFormatado = new File(getClass().getResource("/Code_Smells_MalFormatado.xlsx").getFile());
-	String csFileDefault_MalFormatado = excelDefault_MalFormatado.getAbsolutePath();
+	File excel_MalFormatado = new File(getClass().getResource("/Code_Smells_MalFormatado.xlsx").getFile());
+	String csFile_MalFormatado = excel_MalFormatado.getAbsolutePath();
 	
-	CompareFiles cfSS = new CompareFiles(csFileDefault, csFileCreated);
-	CompareFiles cfSSS = new CompareFiles(csFileDefault, csFileCreated, testeregrasPath);
-	CompareFiles cfSSMau = new CompareFiles(csFileDefault, csFileDefault_MalFormatado);
+	CompareFiles cfSS = new CompareFiles(csFileDefault, csFileCreatedBooleans);
+	CompareFiles cfSSS = new CompareFiles(csFileDefault, csFileCreated_metrics, testeregrasPath);
+	CompareFiles cfSSMalFormatado = new CompareFiles(csFileDefault, csFile_MalFormatado);
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -57,10 +60,10 @@ class CompareFilesTest {
 
 	@Test
 	final void testTestQuality() throws Exception {
-		assertNotNull(cfSS.testQuality(new String[]{"is_god_class","is_long_method"}));
-		assertNotNull(cfSSS.testQuality(new String[]{"is_god_class","is_long_method"}));
+		assertNotNull(cfSS.testQuality());
+		assertNotNull(cfSSS.testQuality());
 		
-		Exception exception = assertThrows(IllegalStateException.class, ()->{cfSSMau.testQuality(new String[]{"is_god_class","is_long_method"});});
+		Exception exception = assertThrows(IllegalStateException.class, ()->{cfSSMalFormatado.testQuality();});
 		String expectedMessage = "Ficheiro mal formatado";
 		String actualMessage = exception.getMessage();
 		assertTrue(actualMessage.contains(expectedMessage));
